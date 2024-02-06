e0693676df7f8ec3c740d2e6965f3cd7a8c02be2
nathanlau <nathanl2026@student.cis.edu.hk>
2024-02-06 11:52:44 +0800

    done

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,63 @@
+// A Student class will have five instance variables. Ensure that each instance variable
+// is declared as private.
+//age (int)
+//firstName (String)
+//lastName (String)
+//year (int)
+//address (Address)
 public class Student {
+    private int age;
+    private String firstName;
+    private String lastName;
+    private int year;
+    private Address address;
+
+    public Student(int age, String firstName, String lastName, int year, Address address) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.address = address;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public int getYear() {
+        return year;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+    private void addOneAge(){
+        age++;
+    }
+    public int birthday(){
+        addOneAge();
+        return age;
+    }
+    @Override
+    public String toString(){
+        return firstName + " " + lastName + ", age " + age + ", year" + year + ", " + address.getStreet() + ", " + address.getDistrict();
+    }
 }
--- src/StudentMaker.java
+++ src/StudentMaker.java
@@ -1,8 +1,16 @@
 public class StudentMaker {
-  
+  ///The district should be Causeway Bay, street Tung Lo Wan Road, and street number 180.
   public static void main(String[] args) {
-    
-    
+  Address address = new Address("Causeway Bay", "Tung Lo Wan Road", 180);
+
+  //The Student’s name should be Belinda Yu, age 17, year 12,
+    // address 180 Tung Lo Wan Road, Causeway Bay. This should be done in a single statement.
+  Student student = new Student(17, "Belinda", "Yu", 12, address);
+  student.setFirstName("Bella");
+    System.out.println(student.getAge());
+    int age = student.birthday();
+    System.out.println(age);
+    System.out.println(student);
   }
   
 }
